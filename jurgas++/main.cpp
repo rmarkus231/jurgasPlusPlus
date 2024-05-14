0f48108e97aca046594c0d70c7205435bb446f4d
Richard Markus Tins <richar03@ut.ee>
2024-05-15 00:45:49 +0300

    threaded sql query

--- jurgas++/main.cpp
+++ jurgas++/main.cpp
@@ -3,27 +3,53 @@
 #include "sqlite/sqlite3.h"
 #include <string.h>
 #include <wx/image.h>
+#include <thread>
 
 using namespace std;
+
+struct sqlThread {
+    //this wasnt actually where the problem was. Off to multithread ui drawing then...
+    string* query;
+    sqlThread(){
+        query = new string;
+    };
+    static int callback(void* data, int argc, char** argv, char** azColName) {
+        for (int i = 0; i < argc; i++) {
+            /*
+            *(static_cast<wxString*>(data)) += wxString(argv[i], wxConvUTF8);
+            *(static_cast<wxString*>(data)) += wxString("<br>", wxConvUTF8);
+            */
+            *(static_cast<string*>(data)) += argv[i];
+            *(static_cast<string*>(data)) += "<br>";
+        };
+        return 0;
+    };
+
+    void GetSql(wxString text,sqlite3* db, int rc, char* zErrMsg, wxHtmlWindow* win) {
+        string sql = "SELECT content FROM questions WHERE question LIKE \'%" + text.ToStdString() + "%\';";
+        rc = sqlite3_exec(db, sql.c_str(), callback, query, &zErrMsg);
+        if (rc) {
+            win->SetPage("<p> Ei suutnud andmebaasi avada. <p>");
+        }
+    }
+};
+
+int threadTask(sqlThread*& sqth, wxString text, sqlite3* db, int rc, char* zErrMsg, wxHtmlWindow*& win) {
+    sqth->GetSql(text, db, rc, zErrMsg, win);
+    return 0;
+};
 
 struct MyFrame : public wxFrame {
     wxHtmlWindow* htmlWindow;
     wxPanel* panel;
     wxTextCtrl* searchCtrl;
     wxBoxSizer* sizer;
-    wxString* str;
 
     sqlite3* db;
     char* zErrMsg = nullptr;
     int rc;
 
-    static int callback(void* data, int argc, char** argv, char** azColName) {
-        for (int i = 0; i < argc; i++) {
-            *(static_cast<wxString*>(data)) += wxString(argv[i], wxConvUTF8);
-            *(static_cast<wxString*>(data)) += wxString("<br>", wxConvUTF8);
-        };
-        return 0;
-    }
+    
 
     MyFrame(const wxString& title)
         : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 600)) {
@@ -36,7 +62,7 @@ struct MyFrame : public wxFrame {
 
         wxInitAllImageHandlers();
 
-        str = new wxString("", wxConvUTF8);
+        //str = new wxString("", wxConvUTF8);
 
         // Set up a sizer to manage layout
         sizer = new wxBoxSizer(wxVERTICAL);
@@ -46,25 +72,16 @@ struct MyFrame : public wxFrame {
         panel->SetSizer(sizer); // Set the sizer for the panel
     }
 
-    void GetSql(wxString text) {
-        string sql = "SELECT content FROM questions WHERE question LIKE \'%" + text.ToStdString() + "%\';";
-        rc = sqlite3_exec(db, sql.c_str(), callback, str, &zErrMsg);
-
-        if (rc) {
-            htmlWindow->SetPage("<p> Ei suutnud andmebaasi avada. <p>");
-        }
-    }
-
     void OnSearch(wxCommandEvent& event) {
-        //make this actually work
         wxString text = searchCtrl->GetValue();
         if (text.length() > 5) {
-            str->clear();
-            GetSql(text);
-            if (str->length() < 2) {
+            sqlThread* sqth = new sqlThread();
+            thread th(threadTask, ref(sqth), text, db, rc, zErrMsg, ref(htmlWindow));
+            th.join();
+            if (sqth->query->length() < 2) {
                 htmlWindow->SetPage("<p> Otsitut ei leitud <p>");
             }else {
-                htmlWindow->SetPage(*str);
+                htmlWindow->SetPage(wxString::FromUTF8(*(sqth->query)));
             }
         }
         else {
